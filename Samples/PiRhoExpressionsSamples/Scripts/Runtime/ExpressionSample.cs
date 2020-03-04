2c483d22d4b0fff941c9114d9d3458e8ea4847b4
Luke Larson <luke.larson@gmail.com>
2020-03-03 17:01:12 -0800

    expression drawer validates correctly

---
+++ Samples/PiRhoExpressionsSamples/Scripts/Runtime/ExpressionSample.cs
@@ -0,0 +1,16 @@
+﻿using PiRhoSoft.Variables;
+using UnityEngine;
+
+namespace PiRhoSoft.Expressions.Samples
+{
+	public class ExpressionSample : MonoBehaviour
+	{
+		[VariableConstraint(0, 10)]
+		public VariableSource Source = new VariableSource();
+
+		public Expression Expression = new Expression();
+		public ReadOnlyExpression ReadOnly = new ReadOnlyExpression();
+		public AssignmentExpression Assignment = new AssignmentExpression();
+		public StringExpression StringExpressions = new StringExpression();
+	}
+}
